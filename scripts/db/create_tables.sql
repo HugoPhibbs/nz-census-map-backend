4d8c61ca7c96b36382688156b919ef2403c3563f
Hugo Phibbs <hugophibbs1@gmail.com>
2026-08-29 14:42:33 +0100

    fixing db compose and create table scripts

--- scripts/db/create_tables.sql
+++ scripts/db/create_tables.sql
@@ -1,14 +1,15 @@
+CREATE TABLE IF NOT EXISTS AREAS(
+    area_code TEXT,
+    area_name TEXT,
+    area_type TEXT,
+    census_year INTEGER,
+    PRIMARY KEY (area_code, census_year)
+);
+
 CREATE TABLE IF NOT EXISTS DEMOGRAPHIC_VARIABLES(
     variable_name TEXT PRIMARY KEY,
     variable_description TEXT,
     variable_unit TEXT
-);
-
-CREATE TABLE IF NOT EXISTS DEMOGRAPHIC_DATA(
-    area_code TEXT,
-    area_type TEXT,
-    census_year INTEGER,
-    PRIMARY KEY (area_code, census_year)
 );
 
 /*
