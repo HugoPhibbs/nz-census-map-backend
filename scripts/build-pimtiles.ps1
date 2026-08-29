21a8bb513ecd63684576476580b20b2d9f24be28
Hugo Phibbs <hugophibbs1@gmail.com>
2026-08-29 12:53:41 +0100

    deleting the build pipelines script

--- scripts/build-pimtiles.ps1
+++
@@ -1,3 +0,0 @@
-docker run --rm -v "${PWD}:/data" -w /data ubuntu:24.04 bash -c "apt update && apt install -y tippecanoe && tippecanoe -f -o area-boundaries.pmtiles -zg --drop-densest-as-needed --extend-zooms-if-still-dropping --coalesce-densest-as-needed -L ta:territorial-authority-2023-clipped-generalised.json -L sa3:statistical-area-3-2023-clipped-generalised.json -L sa2:statistical-area-2-2023-clipped-generalised.json -L coastline:nz-coastlines-and-islands-polygons-topo-1250k.json"
-
-Write-Host "Done. Output: nz-boundaries.pmtiles" -ForegroundColor Green
