eafd455434519a129ca362326b408d84dbf348f0
Hugo Phibbs <hugophibbs1@gmail.com>
2026-08-29 14:41:32 +0100

    improving db setup. creating script for filling the areas table

--- scripts/db_setup.py
+++ scripts/db_setup.py
@@ -1,17 +1,36 @@
 import subprocess
 import argparse
+import json
+import psycopg_pool
 
 def start_db():
-    subprocess.run("docker compose up -d", cwd="./db", shell=True, check=True)
+    subprocess.run("docker compose up -d", cwd="./scripts/db", shell=True, check=True)
 
-def stop_db():
-    subprocess.run("docker compose down", cwd="./db", shell=True, check=True)
-    
+
+def stop_db(reset_vols=False):
+    down_cmd = "docker compose down -v" if reset_vols else "docker compose down"
+    subprocess.run(down_cmd, cwd="./scripts/db", shell=True, check=True)
+
 def fill_variables_table():
     pass
 
 def fill_areas_table():
-    pass
+    with open("./data/all_area_properties.json", "r", encoding="utf-8") as f:
+        all_areas = json.load(f)
+        
+    pool = psycopg_pool.ConnectionPool(conninfo="dbname=census-db user=admin password=devpassword host=localhost port=5432", min_size=1, max_size=10)
+    
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            for area in all_areas:
+                cur.execute(
+                    """
+                    INSERT INTO areas (area_name, area_code, census_year, area_type)
+                    VALUES (%s, %s, %s, %s)
+                    """,
+                    (area["area_name"], area["area_code"], area["census_year"], area["area_type"])
+                )
+    
 
 def fill_demographic_data_table():
     pass
@@ -24,15 +43,18 @@ def fill_tables():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Set up the database using Docker Compose.")
-    parser.add_argument("--start", action="store_true", help="Create the database using Docker Compose.")
-    parser.add_argument("--stop", action="store_true", help="Drop the database using Docker Compose.")
-    parser.add_argument("--fill", action="store_true", help="Fill the database tables with data.")
+    parser.add_argument("--start", default=False, action="store_true", help="Create the database using Docker Compose.")
+    parser.add_argument("--stop", default=False, action="store_true", help="Drop the database using Docker Compose.")
+    parser.add_argument("--fill", default=False, action="store_true", help="Fill the database tables with data.")
+    parser.add_argument("--reset-vols", "-rv", default=False, action="store_true", help="Reset the database by stopping, starting, and filling it.")
     
     args = parser.parse_args()
     
-    if args.start:
+    if args.stop or args.reset_vols:
+        stop_db(args.reset_vols)
+    elif args.start:
         start_db()
-    elif args.stop:
-        stop_db()
     elif args.fill:
         fill_tables()
+    else:
+        print("No action specified. Use --start, --stop, --fill, or --reset-vols.")
