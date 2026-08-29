08a05ada30ec83cdfb5e8b2f5813e131c7b37460
Hugo Phibbs <hugophibbs1@gmail.com>
2026-08-29 12:55:32 +0100

    upgrading the parsing geojson for new keys and handling maori hyphens

--- scripts/parse_geojson.py
+++ scripts/parse_geojson.py
@@ -1,9 +1,9 @@
 import json
 
 files = [
-    {"name": "statistical-area-2-2023-clipped-generalised.json", "id_prefix": "2023S-S2", "existing_id_key": "SA22023__1"},
-    {"name": "statistical-area-3-2023-clipped-generalised.json", "id_prefix": "2023S-S3", "existing_id_key": "SA32023__1"},
-    {"name": "territorial-authority-2023-clipped-generalised.json", "id_prefix": "2023-TA", "existing_id_key": "TA2023_V_1"}
+    {"name": "statistical-area-2-2023-clipped-generalised.json", "id_prefix": "2023-S2", "name_key": "SA22023__1", "id_key": "SA22023_V1"},
+    {"name": "statistical-area-3-2023-clipped-generalised.json", "id_prefix": "2023-S3", "name_key": "SA32023__1", "id_key": "SA32023_V1"},
+    {"name": "territorial-authority-2023-clipped-generalised.json", "id_prefix": "2023-TA", "name_key": "TA2023_V_1", "id_key": "TA2023_V_1"}
 ]
 
 def parse_all_files():
@@ -11,18 +11,20 @@ def parse_all_files():
         with open(f"./data/{file['name']}", "r", encoding="utf-8") as f:
             geojson = json.load(f)
 
-        adjust_properties(geojson, file["id_prefix"], file["existing_id_key"])
+        adjust_properties(geojson, file["id_prefix"], file["name_key"], file["id_key"])
 
         with open(f"./data/{file['name'].split('.')[0]}-adjusted.json", "w", encoding="utf-8") as f:
-            json.dump(geojson, f, indent=2)
+            json.dump(geojson, f, indent=2, ensure_ascii=False)
 
 
-def adjust_properties(geojson, id_prefix, existing_id_key):
+def adjust_properties(geojson, id_prefix, name_key, id_key):
     for feature in geojson["features"]:
         old_properties = feature["properties"]
+        census_area_id = old_properties[id_key]
         new_properties = {
-            "area_id": f"{id_prefix}-{old_properties[existing_id_key]}",
-            "name": old_properties[existing_id_key]
+            "area_id": f"{id_prefix}-{census_area_id}-{old_properties[name_key]}",
+            "name": old_properties[name_key],
+            "census_area_id": census_area_id
         }
         feature["properties"] = new_properties
 
